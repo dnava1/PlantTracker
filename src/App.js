3c0559390078d828820bc95ae8f1a2d89581179f
Daniel Navarro <dnava039@fiu.edu>
2022-04-05 02:20:36 -0400

    Added read from database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState, useEffect} from "react";
 import './App.css';
+import {db} from "./firebaseConfiguration";
+import { collection, getDocs} from "firebase/firestore";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const plantsRef = collection(db, "plants");
+
+  useEffect(() => {
+    const getPlants = async () => {
+      const data = await getDocs(plantsRef);
+      setPlants(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+    };
+    getPlants();
+  });
   return (
     <div className="App">
-     
+      {plants.map((plant) => {
+        return(
+          <div> 
+            {" "}
+            <h1>Name: {plant.name}</h1>
+            <h1>Healh: {plant.health}</h1>
+            <h1>Watered: {plant.watered}</h1>
+          </div>
+        )
+      })}
+      <h1>Name</h1>
+      
     </div>
   );
 }
--- src/firebaseConfiguration.js
+++ src/firebaseConfiguration.js
@@ -1,18 +1,27 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_measurementId,
+
+    apiKey: "AIzaSyDqGgjhMD0C2gqDpulb_wgbwzfPB6Kx_L8",
+  
+    authDomain: "planttracker-8c410.firebaseapp.com",
+  
+    projectId: "planttracker-8c410",
+  
+    storageBucket: "planttracker-8c410.appspot.com",
+  
+    messagingSenderId: "820697267907",
+  
+    appId: "1:820697267907:web:47da938d2ca820775fec76",
+  
+    measurementId: "G-W31YFYZNC0"
+  
   };
   
+  
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  //const analytics = getAnalytics(app);
 
   export const db = getFirestore(app);
